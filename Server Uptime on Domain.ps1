8c39f3d03883d7d3a6c4dfd35b1fd77beba9d7c3
infecticide <tuxsteve@gmail.com>
2014-04-09 12:40:18 -0600

    - Replaced DNS and Ping checks with Powershell / .NET commands for better reliablilty - Improved logging by setting up color coding - Setup script logging for script output

--- Server Uptime on Domain.ps1
+++ Server Uptime on Domain.ps1
@@ -1,52 +1,72 @@
-$ErrorActionPreference = "silentlycontinue"
+Start-Transcript uptime_transcript.log
+
 # Setup CSV
-"Server,Last Boot Up Time,Uptime in Hours" | out-file -file "Server Uptime on $ENV:USERDOMAIN domain.csv"
+"sep=," | Out-File -file "Server Uptime on $ENV:USERDOMAIN domain.csv"
+"Server,Last Boot Up Time,Uptime in Hours" | out-file -file "Server Uptime on $ENV:USERDOMAIN domain.csv" -Append
+
+# Clear variables
 $computer_array = $null
 
 # Get list of all computer object in current domain
+Write-Host "Getting server list from domain $ENV:USERDOMAIN"
 $objDomain = New-Object System.DirectoryServices.DirectoryEntry
 $objSearcher = New-Object System.DirectoryServices.DirectorySearcher
 $objSearcher.PageSize = 10000
 $objSearcher.SearchRoot = $objDomain
 $objSearcher.Filter = ("(OperatingSystem=Window*Server*)")
-$objSearcher.PropertiesToLoad.Add("name") | out-null
+$objSearcher.PropertiesToLoad.Add("name") | Out-Null
 $colResults = $objSearcher.FindAll()
 foreach ($objResult in $colResults) {
     $objComputer = $objResult.Properties
     $computer_array += $objComputer.name
     }
 
-$computercount = $computers.Count
+$computercount = $computer_array.Count
 write-host "$computercount computers found"
 
 # Query Servers
 foreach ($machine in $computer_array) {
-    Write-Output "Checking $machine"
+    Write-Output "$machine - Looking up hostname"
     # Is machine in DNS?
-    nslookup $machine | out-null
-    if($? -eq $false) {
+    $ErrorActionPreference = "silentlycontinue"
+    $lookup = [System.Net.Dns]::GetHostAddresses($machine)
+    $ErrorActionPreference = "continue"
+    if($lookup -eq $null) {
         "$machine,not found in DNS" | Out-File -file "Server Uptime on $ENV:USERDOMAIN domain.csv" -Append
         $computercount = $computercount - 1
-        Write-Output "Finished checking $machine"
-        write-host "$computercount computers left to check"
+        Write-Host "$machine - Hostname not found" -ForegroundColor Red
+        Write-Host "$computercount computers left to check"
         continue
-        }
+        } ELSE {
+        Write-Host "$machine - DNS lookup successful" -ForegroundColor Green
+        } # End nslookup IF
     # Is machine reachable?
-    ping -n 1 $machine | out-null
-    if($? -eq $false) {
+    if(test-connection -ComputerName $machine -Count 1 -Quiet) {
+        Write-Host "$machine - is reachable" -ForegroundColor Green
+        } ELSE {
         "$machine,not reachable" | Out-File -file "Server Uptime on $ENV:USERDOMAIN domain.csv" -Append
-        Write-Output "Finished checking $machine"
         $computercount = $computercount - 1
-        write-host "$computercount computers left to check"
+        Write-Host "$machine - did not respond" -ForegroundColor Red
+        Write-Host "$computercount computers left to check"
         continue
         }
     # Get uptime
-    $wmi = Get-WmiObject -ComputerName $machine -Class Win32_OperatingSystem
+    $wmi = Get-WmiObject -ComputerName $machine -Class Win32_OperatingSystem -ea silentlycontinue
+    if($? -eq $false) {
+        "$machine,WMI encountered an error" | Out-File -File "Server Uptime on $ENV:USERDOMAIN domain.csv" -Append
+        $computercount = $computercount - 1
+        Write-Host "$machine - encountered an error in WMI" -ForegroundColor Red
+        Write-Host "$computercount computers left to check"
+        continue
+        } ELSE {
+        Write-Host "$machine - queried successfully" -ForegroundColor Green
+        } # End WMI IF
     $lastbootuptime = $wmi.ConvertToDateTime($wmi.LastBootUpTime)
     $difference = ($wmi.ConvertToDateTime($wmi.LocalDateTime) – $wmi.ConvertToDateTime($wmi.LastBootUpTime))
     $hours_up = $difference.TotalHours
     # Output to CSV
-    "$machine,$lastbootuptime,$hours_up" | out-file -File "Server Uptime on $ENV:USERDOMAIN domain.csv" -Append
+    "$machine,$lastbootuptime,$hours_up" | Out-File -File "Server Uptime on $ENV:USERDOMAIN domain.csv" -Append
     $computercount = $computercount - 1
-    write-host "$computercount computers left to check"
-}
+    Write-Host "$computercount computers left to check"
+} # End ForEach $machine IF
+Stop-Transcript
